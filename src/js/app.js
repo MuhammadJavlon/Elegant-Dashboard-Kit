144c65f0f0e33e6d753b1dd3bf8df58558f9fafe
Jamshid Tashpulatov <44395036+JamshidTashpulatov@users.noreply.github.com>
2021-05-21 18:38:41 +0500

    Uploaded

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import "../scss/app.scss";
+
+import "./modules/bootstrap";
+import "./modules/sidebar";
+import "./modules/custom";
+import "./modules/theme";
+import "./modules/feather";
+
+// Charts
+import "./modules/chartjs";
+
+// Forms
+import "./modules/flatpickr";
+
+// Maps
+import "./modules/vector-maps";
+
+// Dark/Light Switcher
+import "./modules/dark-mode-switch";
